3354155b914d70dc53b4ab19b45a71442fc2a900
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-07 14:07:44 +0545

    updated

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ app.get('/', (req, res)=>{
 })
 
 io.on('connection', (socket)=>{
-    console.log('a new client connected')
-    socket.on('disconnected',()=>{
-        console.log('client disconnected')
+    socket.on('chat message',(msg)=>{
+        console.log('message:'+msg)
     })
 })
 
