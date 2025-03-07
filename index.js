6387f7ed99f0c3b03a4a8844ed8da36f499b1ed8
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-07 12:21:40 +0545

    updated

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const {createServer}= require('node:http')
 
-const app = express()
+const app = express();
+const server = createServer(app);
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Hello World!</h1>')
+})
+
