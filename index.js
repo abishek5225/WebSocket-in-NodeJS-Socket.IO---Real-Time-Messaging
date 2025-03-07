ea9b0f352b146d0f7905dcc9646adc2bac32eb0c
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-07 13:54:56 +0545

    updated

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ import express from 'express'
 import {createServer} from 'node:http'
 import { fileURLToPath } from 'node:url';
 import {dirname , join} from 'node:path';
-
+import {Server} from 'socket.io'
 
 const app = express();
 const server = createServer(app);
+const io= new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const __client = join(__dirname, 'client')
 
 app.get('/', (req, res)=>{
     res.sendFile(join(__client, 'index.html'))
+})
+
+io.on('connection', (socket)=>{
+    console.log('a new client connected')
 })
 
 server.listen(3500, ()=>{
