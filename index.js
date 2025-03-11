ad1db143749410d634a6219adb1344abb6e8dcf5
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-11 20:35:37 +0545

    updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {createServer} from 'node:http'
 import { fileURLToPath } from 'node:url';
 import {dirname , join} from 'node:path';
 import {Server} from 'socket.io'
+import { log } from 'node:console';
 
 const app = express();
 const server = createServer(app);
@@ -15,7 +16,10 @@ app.get('/', (req, res)=>{
     res.sendFile(join(__client, 'index.html'))
 })
 
-
+//implementing socket
+io.on('connection', (socket)=>{
+    console.log('a user connected');
+})
 
 server.listen(3500, ()=>{
     console.log('Server is running on port http://localhost:3500')
